885a49a5d8f203c6b8ced0490c639d3939a8ce46
Snezhana <89837216+sskrolkina@users.noreply.github.com>
2022-11-03 13:16:25 +0300

    Update 2.c

--- Task_7/2.c
+++ Task_7/2.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
-struct Square{
-    int side;
+
+struct Square
+{
+    int x[2];
+    int y[2];
+    int z[2];
+    int k[2];
+
 };
 
-int main() {
-    struct Square a;
-    scanf("%d",&a.side);
-    printf("Side of square = %d\n",a);
-    printf("P = %d\n",(4*a.side));
-    printf("S = %d",(a.side * a.side));
+int P(int x)
+{
+    return x * 4;
+}
+int S(int x){
+    return x * x;
+}
 
+int main(){
+
+    struct Square coordinate = {.x = {0, 0}, .y = {0, 2}, .z = {2, 2}, .k = {2, 0} };
+    int sqr = coordinate.y[1] - coordinate.x[1];
+
+    printf("P = %d\n",P(sqr));
+    printf("S = %d\n",S(sqr));
+    
     return 0;
 }
