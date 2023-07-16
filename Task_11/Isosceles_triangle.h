9989cdd2ac004c5605b9019fd38c169c2ae4e618
Snezhana <89837216+sskrolkina@users.noreply.github.com>
2023-07-17 01:06:27 +0300

    Create Isosceles_triangle.h

---
+++ Task_11/Isosceles_triangle.h
@@ -0,0 +1,17 @@
+#ifndef KILLME__ISOSCELES_TRIANGLE_H_
+#define KILLME__ISOSCELES_TRIANGLE_H_
+	
+
+	struct coords{
+	  float x;
+	  float y;
+	};
+	
+
+	float Perimeter(struct coords,struct coords,struct coords);
+	
+
+	float Area(struct coords,struct coords,struct coords);
+	
+
+	#endif
